cd67b6995ea139c2b6e331caf3872dd064978b28
francescobianco <info.francescobianco@gmail.com>
2017-07-25 23:28:36 +0200

    first commit 2

---
+++ app/config.js
@@ -0,0 +1,10 @@
+
+var config = {
+    client_id: "989152715769-5dmdgb43mtqhup9vfglu00qagguqstn4.apps.googleusercontent.com",
+    discovery: ["https://people.googleapis.com/$discovery/rest"],
+    scopes: [
+        "profile",
+        "https://www.googleapis.com/auth/tasks",
+        "https://www.googleapis.com/auth/contacts.readonly"
+    ]
+}
--- src/app.js
+++ src/app.js
@@ -1,13 +1,4 @@
 
-//
-var CLIENT_ID = "989152715769-5dmdgb43mtqhup9vfglu00qagguqstn4.apps.googleusercontent.com";
-var DISCOVERY = ["https://people.googleapis.com/$discovery/rest"];
-var SCOPES = [
-    "profile",
-    "https://www.googleapis.com/auth/tasks",
-    "https://www.googleapis.com/auth/contacts.readonly"
-];
-
 
 
 //
