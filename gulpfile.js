be7a102f8df59e27f00f34f2ef7a0dfcab9b76d2
francescobianco <info.francescobianco@gmail.com>
2017-07-26 01:20:49 +0200

    first commit 3

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,45 @@ var concat = require("gulp-concat");
 var concatCss = require("gulp-concat-css");
 var modifyCssUrls = require("gulp-modify-css-urls");
 var templateCache = require("gulp-angular-templatecache");
+var runSequence = require('run-sequence');
 var basename = require("path").basename;
 
-gulp.task("default", function() {
-    gulp.src(["./style/uikit.css", "./style/**/*.css"])
+gulp.task("default", function(done) {
+    runSequence("style", "fonts", "html", "app", done);
+});
+
+gulp.task("style", function() {
+    return gulp.src(["./style/uikit.css", "./style/**/*.css"])
         .pipe(concatCss("style.css"))
-        .pipe(modifyCssUrls({ modify: function (url, filePath) { return "../fonts/" + basename(url); } }))
-        .pipe(gulp.dest("./css"));
-    gulp.src("./bower_components/**/fonts/*.{ttf,woff,eof,eot,svg,htc}")
-        .pipe(rename({ dirname: '' }))
-        .pipe(gulp.dest("./fonts"));
-    gulp.src("./app/**/*.html")
+        .pipe(modifyCssUrls({
+            modify: function (url, filePath) {
+                return "../fonts/" + basename(url);
+            }
+        }))
+        .pipe(gulp.dest("./public/css"));
+});
+
+gulp.task("fonts", function() {
+    return gulp.src("./bower_components/**/fonts/*.{ttf,woff,eof,eot,svg,htc}")
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest("./public/fonts"));
+});
+
+gulp.task("html", function() {
+    return gulp.src("./app/**/*.html")
         .pipe(templateCache({root: "app/", module: "app"}))
         .pipe(gulp.dest("app"));
-    gulp.src(["./bower_components/**/*.min.js", "./app/**/*.js"])
-        .pipe(concat("app.js"))
-        .pipe(gulp.dest("./js"));
+});
+
+gulp.task("app", function() {
+    return gulp.src([
+        "./bower_components/jquery/dist/jquery.min.js",
+        "./bower_components/angular/angular.min.js",
+        "./bower_components/angular-ui-router/release/angular-ui-router.min.js",
+        "./app/app.js",
+        "./app/templates.js",
+        "./app/**/*.js"
+    ])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("./js"));
 });
