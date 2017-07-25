e59d83388e770fa230562c9415e123fb42337a29
francescobianco <info.francescobianco@gmail.com>
2017-07-25 23:36:48 +0200

    first commit 2

---
+++ app/comp/layout/layout.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ */
+
+angular
+    .module("app")
+    .component("layout", {
+        templateUrl: "app/components/layout/layout.html",
+        controller: function($scope) {}
+    });
