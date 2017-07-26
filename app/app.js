cb37db1e9d945f7b19f4c55919afce2d34032b8d
francescobianco <info.francescobianco@gmail.com>
2017-07-26 02:31:28 +0200

    first commit 3

--- app/app.js
+++ app/app.js
@@ -3,6 +3,11 @@
  */
 angular
     .module("app", ["ui.router"])
-    .config(function() {
-
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state("home", {
+                url: "/",
+                template: "<board/>"
+            });
     });
