279f321e352217e3cd1c1a7db2749afb4b753c89
francescobianco <info.francescobianco@gmail.com>
2017-07-26 02:02:27 +0200

    first commit 3

---
+++ app/comp/board/board.js
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+
+angular
+    .module("app")
+    .component("board", {
+        templateUrl: "app/comp/board/board.html",
+        controller: function($scope) {}
+    });
---
+++ app/comp/header/header.js
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+
+angular
+    .module("app")
+    .component("header", {
+        templateUrl: "app/comp/header/header.html",
+        controller: function($scope) {}
+    });
